a36c336ba6404be3a706b7c0654ed1945e15f218
Arthur Turlak <arthur@lunarcharc.com>
2018-12-21 09:44:35 -0700

    adding notes to full array output

--- index.php
+++ index.php
@@ -31,7 +31,12 @@ DB::$dbName = 'englund';
 
 
 /*General Select */
-$sku = 'RUL10';
+//$sku = 'RUL10';
+
+//build a list of skus
+//TODO: Build this list from another query or a some form input
+$skus = array('BLU','RUL10');
+
 //$results = DB::query("SELECT * FROM dw_item WHERE dwin_item_number = %s",$sku);
 ////print_r($results);
 //
@@ -52,44 +57,73 @@ $sku = 'RUL10';
 //
 //}
 
+//cycle through all the skus
+foreach ($skus as $sku){
+
 //initialize $note;
-$note = '';
+	$note = '';
 
 //this will output a note (aka detailed description) for the product
 //each db row is an individual row of the description.
-$notes = DB::query("SELECT mx_text FROM view_item_notes WHERE mg_group_name = %s ORDER BY mx_line_nbr",$sku);
-foreach ($notes as $row) {
+	$notes = DB::query("SELECT mx_text FROM view_item_notes WHERE mg_group_name = %s ORDER BY mx_line_nbr",$sku);
+	foreach ($notes as $row) {
 
-	foreach ($row as $key=>$val){
-		//concatenate each row's note value
-		//add space to end of each line to ensure a space exists between words
-		$note .= $val." ";
+		foreach ($row as $key=>$val){
+			//concatenate each row's note value
+			//add space to end of each line to ensure a space exists between words
+			$note .= $val." ";
 
-	//	print $key.":"  . $val . "<br/>";
+			//	print $key.":"  . $val . "<br/>";
+
+		}
+
+//	print_R($row);
+//	print "\n";
+	}
+
+
+//<xmp> tags will show raw html
+	print '<xmp>';
+//print_R($note);
+	print '</xmp>';
+
+
+//TODO: get all child products under the
+	$results = DB::query(
+			"SELECT * FROM `dw_item` INNER JOIN `in` ON `dw_item`.`dwin_item_number`=`in`.`in_item_number` AND `dw_item`.`dwin_store`=`in`.`in_store` INNER JOIN `view_dw_department` ON `dw_item`.`dwin_store`=`view_dw_department`.`dwde_store_number` AND `dw_item`.`dwin_department`=`view_dw_department`.`dwde_department` INNER JOIN `view_dw_class` ON `dw_item`.`dwin_store`=`view_dw_class`.`dwcl_store` AND `dw_item`.`dwin_class`=`view_dw_class`.`dwcl_class` INNER JOIN `view_dw_fineline` ON `dw_item`.`dwin_store`=`view_dw_fineline`.`dwfi_store` AND `dw_item`.`dwin_fineline`=`view_dw_fineline`.`dwfi_fineline_code`INNER JOIN `view_dw_vendor` ON `dw_item`.`dwin_store`=`view_dw_vendor`.`dwvm_store` AND `dw_item`.`dwin_primary_vendor`=`view_dw_vendor`.`dwvm_vendor_code`INNER JOIN `view_dw_manufacturer` ON `dw_item`.`dwin_store`=`view_dw_manufacturer`.`dwvm_store` AND `dw_item`.`dwin_manufacturer`=`view_dw_manufacturer`.`dwvm_vendor_code` WHERE dwin_item_number = %s",$sku);
+
+	foreach ($results as $key=>$row){
+
+		$notes = DB::query("SELECT mx_text FROM view_item_notes WHERE mg_group_name = %s ORDER BY mx_line_nbr",$sku);
+		foreach ($row as $key1=>$val){
+			//concatenate each row's note value
+			//add space to end of each line to ensure a space exists between words
+			$note .= $val." ";
+
+			//	print $key.":"  . $val . "<br/>";
+
+		}
+		$results[$key]['item_notes_concat'] = '<xmp>'.$note.'</xmp>';
+
 
 	}
 
 //	print_R($row);
 //	print "\n";
-}
+//}
 
 
 //<xmp> tags will show raw html
-print '<xmp>';
+//print '<xmp>';
 //print_R($note);
-print '</xmp>';
-
-
-//TODO: get all child products under the
-$results = DB::query(
-		"SELECT * FROM `dw_item` INNER JOIN `in` ON `dw_item`.`dwin_item_number`=`in`.`in_item_number` AND `dw_item`.`dwin_store`=`in`.`in_store` INNER JOIN `view_dw_department` ON `dw_item`.`dwin_store`=`view_dw_department`.`dwde_store_number` AND `dw_item`.`dwin_department`=`view_dw_department`.`dwde_department` INNER JOIN `view_dw_class` ON `dw_item`.`dwin_store`=`view_dw_class`.`dwcl_store` AND `dw_item`.`dwin_class`=`view_dw_class`.`dwcl_class` INNER JOIN `view_dw_fineline` ON `dw_item`.`dwin_store`=`view_dw_fineline`.`dwfi_store` AND `dw_item`.`dwin_fineline`=`view_dw_fineline`.`dwfi_fineline_code`INNER JOIN `view_dw_vendor` ON `dw_item`.`dwin_store`=`view_dw_vendor`.`dwvm_store` AND `dw_item`.`dwin_primary_vendor`=`view_dw_vendor`.`dwvm_vendor_code`INNER JOIN `view_dw_manufacturer` ON `dw_item`.`dwin_store`=`view_dw_manufacturer`.`dwvm_store` AND `dw_item`.`dwin_manufacturer`=`view_dw_manufacturer`.`dwvm_vendor_code` WHERE dwin_item_number = %s",$sku);
-
-print'<pre>';
-print_R($results);
-print'</pre>';
+//print '</xmp>';
 
 
 //foreach ($results as $row) {
+
+
+
+
 //	$rows[] = $row;
 ////	print '<xmp>';
 //	print'<pre>';
@@ -100,4 +134,12 @@ print'</pre>';
 //}
 //var_dump($rows);
 
+	print'<pre>';
+	print_R($results);
+	print'</pre>';
+
+
+
+
+}
 
